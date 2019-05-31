73a630e3780f82dc65b6b140e0f732919fb4046d
Richard-Walter <willthisnamedo@gmail.com>
2019-05-31 11:22:33 +1000

    add first look at Promises

--- Async JavaScript/sandbox.js
+++ Async JavaScript/sandbox.js
@@ -9,37 +9,75 @@
 // console.log(4)
 
 /*       HTTP Requests       */
-const getTodos = (resource, callback) => {
-    
-    const request = new XMLHttpRequest();
+const getTodos = (resource) => {
 
-    //request  goes through 4 state changes  opened, headers, received, loading, done
-    request.addEventListener('readystatechange', () => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        //request  goes through 4 state changes  opened, headers, received, loading, done
+        request.addEventListener('readystatechange', () => {
             // console.log(request, request.readyState);
             if (request.readyState === 4 && request.status === 200) { //Request done and No Error
                 const data = JSON.parse(request.responseText)   //convert JSON to an JS array
-                callback(undefined, data);
-            } else if  (request.readyState === 4) {
-                callback("could not find data", undefined);
+                resolve(data)
+            } else if (request.readyState === 4) {
+                reject("error getting resource");
             }
-    })
+        })
 
-    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.open('GET', resource);
-    request.send()
+        // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+        request.open('GET', resource);
+        request.send()
+    });
+    ;
 }
 
-console.log(1)
-console.log(2)
+getTodos('luigi.json').then(data => {
+    console.log('promise resolve', data)
+}).catch(err => {
+    console.log("promise rejected", err)
+})
 
-getTodos('luigi.json', (err, data) => {
-    console.log('Callback Fired')
-    if (err){
-        console.log(err)
-    } else {
-        console.log(data)
-    }
-});
 
-console.log(3)
-console.log(4)
+
+
+// //callback hell!!!
+// getTodos('luigi.json', (err, data) => {
+//     console.log(data);
+//     getTodos('mario.json', (err, data) => {
+//       console.log(data);
+//       getTodos('shaun.json', (err, data) => {
+//         console.log(data);
+//       });
+//     });
+//   });
+
+/*     PROMISE EXAMPLE           */
+
+// const getSomething = () => {
+
+//     //promise to do something but takes time - leads to resolved or rejected due to error 
+//     return new Promise((resolve, reject) => {   //resolve, reject and builtin promise functions
+//         //fetch something
+//         // resolve('somedata') //this then calls the then() function
+//         reject('some error')
+//     });
+// }
+
+// console.log(getSomething());
+
+//then() method takes 2 call back functions for resolve(arg) and reject(arg)
+// getSomething().then((data) => {
+//     console.log(data)
+
+// }, (err) => {
+//     console.log(err)
+// });
+
+//neater way than above
+// getSomething().then(data => {
+//     console.log(data)
+
+// }).catch(err => {
+//     console.log(err)
+// })
