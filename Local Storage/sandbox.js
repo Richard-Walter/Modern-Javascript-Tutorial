139baf4d5aa7de87287682430ed246414ac275e1
Richard-Walter <willthisnamedo@gmail.com>
2019-06-03 20:58:51 +1000

    ads storing and parsing JSON type data to local storage

--- Local Storage/sandbox.js
+++ Local Storage/sandbox.js
@@ -23,3 +23,18 @@ console.log(localStorage.getItem('name'))   //returns null
 
 //remove all items from localstorage
 localStorage.clear();
+
+//storing more complex data
+const todos = [
+    {text: 'play mariokart', author: 'shaun'},
+    {text: 'buy some milk', author: 'mario'},
+    {text: 'buy some bread', author: 'luigi'}
+  ];
+  
+//   console.log(JSON.stringify(todos));
+  localStorage.setItem('todos', JSON.stringify(todos));
+  
+  const stored = localStorage.getItem('todos');
+  // console.log(stored);
+  
+  console.log(JSON.parse(stored));
