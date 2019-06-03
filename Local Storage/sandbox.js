431710b97fa8dfbc7c309576b017ecde86d6e8e9
Richard-Walter <willthisnamedo@gmail.com>
2019-06-03 20:44:01 +1000

    updating, deleting and clearing from local storage

--- Local Storage/sandbox.js
+++ Local Storage/sandbox.js
@@ -1,4 +1,25 @@
-//store data in local storage
+//store data in local storage - data stays after a refresh or new tab
 console.log('test')
+
+// can see this data in the dev console  uner application-> Local storage
 localStorage.setItem('name', 'mario')
-localStorage.setItem('age', 50)
+localStorage.setItem('age', 50) //everything in local storage will get converted to string
+
+//get data from local storage
+let name = localStorage.getItem('name')
+let age = localStorage.getItem('age')
+console.log(name)
+console.log(age)
+
+//updating data
+localStorage.setItem('name', 'luigi')
+localStorage.age = '40'
+console.log(localStorage.getItem('name'))
+console.log(localStorage.getItem('age'))
+
+//deleting data from local storage
+localStorage.removeItem('name')
+console.log(localStorage.getItem('name'))   //returns null
+
+//remove all items from localstorage
+localStorage.clear();
