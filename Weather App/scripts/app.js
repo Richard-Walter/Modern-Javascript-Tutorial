e1aff37c42cfa2017a24fb778889f52c31baa083
Richard-Walter <willthisnamedo@gmail.com>
2019-06-05 11:51:43 +1000

    create the weather class

--- Weather App/scripts/app.js
+++ Weather App/scripts/app.js
@@ -1,19 +1,21 @@
+/* eslint-disable spaced-comment */
+/* eslint-disable indent */
 const cityForm = document.querySelector('form')
 const card = document.querySelector('.card')
 const details = document.querySelector('.details')
 const time = document.querySelector('img.time')
 const icon = document.querySelector('.icon img')
+const weather = new Weather()
 
 const updateUI = (data) => {
+  // const cityDets = data.cityDets;
+  // const weather = data.weather;
 
-    // const cityDets = data.cityDets;
-    // const weather = data.weather;
+  // destructure properties of above
+  const { cityDets, weather } = data
 
-    //destructure properties of above
-    const { cityDets, weather } = data
-
-    console.log(details)
-    details.innerHTML = `
+  console.log(details)
+  details.innerHTML = `
         <h5 class="my-3">${cityDets.EnglishName}</h5>
         <div class="my-3">${weather.WeatherText}</div>
         <div class="display-4 my-4">
@@ -21,64 +23,48 @@ const updateUI = (data) => {
             <span>&deg;C</span>
         </div>`
 
-    //update night and day images
-    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
-    icon.setAttribute('src', iconSrc)
+  // update night and day images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+  icon.setAttribute('src', iconSrc)
 
-    // let timeSrc = null;
-    // if (weather.IsDayTime){
-    //     timeSrc = 'img/day.svg'
-    // } else{
-    //     timeSrc = 'img/night.svg'
-    // }
+  // let timeSrc = null;
+  // if (weather.IsDayTime){
+  //     timeSrc = 'img/day.svg'
+  // } else{
+  //     timeSrc = 'img/night.svg'
+  // }
 
-    //use ternary operator for above commented code
-    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg'
+  // use ternary operator for above commented code
+  let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg'
 
-    time.setAttribute('src', timeSrc)
+  time.setAttribute('src', timeSrc)
 
-
-    //remove the d-none class if present
-    if (card.classList.contains('d-none')) {
-        card.classList.remove('d-none');
-    }
-
+  // remove the d-none class if present
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none')
+  }
 }
-
-
-const updateCity = async (city) => {    //returns a promise due to async keyword
-
-    const cityDets = await getCity(city);   //wait unit we get city before calling next function
-    const weather = await getWeather(cityDets.Key)
-
-    // return {cityDets: cityDets, weather: weather}
-    //object short-hand notation: simpfly above statement
-    return { cityDets, weather };
-
-}
-
 
 cityForm.addEventListener('submit', e => {
-    //prevent default actions
-    e.preventDefault();
+  // prevent default actions
+  e.preventDefault()
 
-    //get city value
-    const city = cityForm.city.value.trim();
-    cityForm.reset();
+  //get city value
+  const city = cityForm.city.value.trim()
+  cityForm.reset()
 
-    //update UI with new city
-    updateCity(city)
-        .then(data => updateUI(data))
-        .catch(err => console.log(err));
+  //update UI with new city
+  weather.updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err))
 
-    //set city to local storage
-    localStorage.setItem('city', city)
-
-});
+  //set city to local storage
+  localStorage.setItem('city', city)
+})
 
 // This will run everythime the user visits the page or re-freshers the page
 if (localStorage.getItem('city')) {
-    updateCity(localStorage.getItem('city'))
-        .then(data => updateUI(data))
-        .catch(err => console.log(err));
+  weather.updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err))
 }
--- Weather App/scripts/weather.js
+++ Weather App/scripts/weather.js
@@ -1,30 +1,31 @@
-const key = 'vd10BzG5pEfSLGAqEoX6krNtTv1bO5PR'
+class Weather {
+  constructor() {
+    this.key = 'vd10BzG5pEfSLGAqEoX6krNtTv1bO5PR'
+    this.WeatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+  }
 
-// get weather information
-const getWeather = async (id) => {
-  
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
-  
-    const response = await fetch(base + query);
-    const data = await response.json();
-  
-    return data[0];
-  
-  };
-  
-  // get city information
-  const getCity = async (city) => {
-  
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-  
-    const response = await fetch(base + query);
-    const data = await response.json();
-  
-    return data[0];
-  
-  };
-  
+  async updateCity(city) {
+    // returns a promise due to async keyword
+    const cityDets = await this.getCity(city) // wait unit we get city before calling next function
+    const weather = await this.getWeather(cityDets.Key)
 
+    // return {cityDets: cityDets, weather: weather}
+    // object short-hand notation: simpfly above statement
+    return { cityDets, weather }
+  }
+  async getCity(city) {
+    const query = `?apikey=${this.key}&q=${city}`
+    const response = await fetch(this.cityURI + query)
+    const data = await response.json()
 
+    return data[0]
+  }
+
+  async getWeather(id) {
+    const query = `${id}?apikey=${this.key}`
+    const response = await fetch(this.WeatherURI + query)
+    const data = await response.json()
+    return data[0]
+  }
+}
