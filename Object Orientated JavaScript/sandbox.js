d519944b7bb42aa17d2b09c8caa1fac1bfb241c1
Richard-Walter <willthisnamedo@gmail.com>
2019-06-05 10:07:33 +1000

    add class inheritance and super()

--- Object Orientated JavaScript/sandbox.js
+++ Object Orientated JavaScript/sandbox.js
@@ -1,3 +1,4 @@
+/* eslint-disable padded-blocks */
 /* eslint-disable space-before-blocks */
 /* eslint-disable space-before-function-paren */
 /* eslint-disable indent */
@@ -5,23 +6,23 @@
 // classes
 class User {
     // constructor
-    constructor(username, email){
+    constructor(username, email) {
         this.username = username
         this.email = email
         this.score = 0
     }
 
     // methods - note arrow functions don't bind to this keyword
-    login(){
+    login() {
         console.log(`${this.username} just logged in`)
         return this
     }
-    logout(){
+    logout() {
         console.log(`${this.username} just logged out`)
         return this
     }
 
-    incScore(){
+    incScore() {
         this.score += 1
         console.log(`${this.username} has a score of ${this.score}`)
         return this
@@ -31,9 +32,30 @@ class User {
 const userOne = new User('shaun', 'shaun@thenetninja.co.uk')
 const userTwo = new User('Rich', 'rich@thenetninja.co.uk')
 
-console.log(userOne, userTwo)
 userOne.login()
 
 // method chaining
 userOne.incScore().incScore()
 userTwo.login().incScore().logout()
+
+// class inheritance
+class Admin extends User {
+
+    constructor(username, email, title){
+        super(username, email)
+        this.title = title
+    }
+
+    deleteUser() {
+        users = users.filter(u => u.username !== userOne.username) // false will filter out user
+    }
+}
+
+const userThree = new Admin('Admin', 'admin@gmail.com', 'black-belt-ninja')
+
+console.log(userOne, userTwo, userThree)
+
+let users = [userOne, userTwo, userThree]
+console.log(users)
+userThree.deleteUser(userTwo)
+console.log(users)
