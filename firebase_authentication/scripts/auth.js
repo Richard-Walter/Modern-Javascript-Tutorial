9711751f7dc447c0dcb824b65847c61dd39c571c
Richard-Walter <willthisnamedo@gmail.com>
2019-06-10 11:32:31 +1000

    add login logout functionality

--- firebase_authentication/scripts/auth.js
+++ firebase_authentication/scripts/auth.js
@@ -1,3 +1,15 @@
+// listen to auth status changes.  
+auth.onAuthStateChanged((user) => {
+  if (user) { //if not logged in the user object is null
+    console.log('user logged in : ', user); 
+  } else {
+    console.log('User has logged out');
+  }
+ })
+
+
+
+
 //signup
 const signupForm = document.querySelector('#signup-form')
 
@@ -11,14 +23,37 @@ signupForm.addEventListener('submit', e => {
   // sign up the user using firebase auth object we created in the HTML
   //this is asyncronous and returns a promise containing a user credential
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred);
+    
     const modal = document.querySelector('#modal-signup')
     //close the signup modal
     M.Modal.getInstance(modal).close()
     signupForm.reset()
-    
-  })
 
+  })
+})
+
+// logout
+const logout = document.querySelector('#logout')
+logout.addEventListener('click', e => {
+  e.preventDefault();
+  auth.signOut()
 
 })
 
+// log in
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = loginForm['login-email'].value
+  const password = loginForm['login-password'].value
+
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+   
+    //close the signup modal
+    const modal = document.querySelector('#modal-login')
+    M.Modal.getInstance(modal).close()
+    signupForm.reset()
+  })
+
+})
