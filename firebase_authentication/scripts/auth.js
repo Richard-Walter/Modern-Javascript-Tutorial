ef01d584c097449f679e3c74840b1396e5b339b3
Richard-Walter <willthisnamedo@gmail.com>
2019-06-13 13:59:08 +1000

    add create guide functionality

--- firebase_authentication/scripts/auth.js
+++ firebase_authentication/scripts/auth.js
@@ -61,3 +61,20 @@ loginForm.addEventListener('submit', e => {
   })
 
 })
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('guides').add({
+    title: createForm.title.value,
+    content: createForm.content.value
+  }).then(() => {
+    // close the create modal & reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err => { //such as user doesn;t have permission
+    console.log(err.message);
+  });
+});
